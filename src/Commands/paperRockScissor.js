3fb04f0fb90785c0455fc8981607f02c6b1c6ca9
Kruk1 <albertkr@wp.pl>
2023-03-01 15:48:12 +0100

    Add embed to rock-paper-scissors command

--- src/Commands/paperRockScissor.js
+++ src/Commands/paperRockScissor.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, bold } = require("@discordjs/builders");
+const { SlashCommandBuilder, bold, EmbedBuilder} = require("@discordjs/builders");
 const options = ['rock', 'paper', 'scissors']
 
 
@@ -24,8 +24,8 @@ module.exports = {
 
     const filter = response => {
         if(interaction.options.getUser('user'))
-            return options.some(option => option.toLowerCase() === response.content.toLowerCase()) && interaction.options.getUser('user').id === response.author.id;
-        return options.some(option => option.toLowerCase() === response.content.toLowerCase());
+            return options.some(option => option.toLowerCase() === response.content.toLowerCase()) && interaction.options.getUser('user').id === response.author.id && response.author.id !== interaction.user.id;
+        return options.some(option => option.toLowerCase() === response.content.toLowerCase()) && response.author.id !== interaction.user.id;
     };
 
     if(!interaction.options.getUser('user'))
@@ -72,36 +72,46 @@ module.exports = {
         const user = interaction.options.getUser('user')
         const choice = interaction.options.getString('choice')
 
+        if(user.id === interaction.user.id) return interaction.reply(bold(`You cant play with yourself!`))
+
         await interaction.reply(`Waiting for <@${user.id}>...`)
         .then(() =>
         {
             interaction.channel.awaitMessages({ filter, max: 1, time: 10000, errors: ['time'] })
                 .then(collected => {
+                    let winner = collected.first().author.id
+                    const drawEmbed = new EmbedBuilder()
+                    .setColor(0x0099FF)
+                    .setTitle('Draw!')
                     if(choice === collected.first().content) {
-                        return interaction.followUp(bold(`Draw! Play again`))
+                        return interaction.channel.send({ embeds: [drawEmbed] })
                     }
                     else if(choice === 'rock')
                     {
                         if(collected.first().content !== 'paper')
                         {
-                            return interaction.followUp(`Winner is <@${interaction.user.id}>!`)
+                            winner = interaction.user.id
                         }
                     }
                     else if(choice === 'paper')
                     {
                         if(collected.first().content !== 'scissors')
                         {
-                            return interaction.followUp(`Winner is <@${interaction.user.id}>!`)
+                            winner = interaction.user.id
                         }
                     }
                     else if(choice === 'scissors')
                     {
                         if(collected.first().content !== 'rock')
                         {
-                            return interaction.followUp(`Winner is <@${interaction.user.id}>!`)
+                            winner = interaction.user.id
                         }
                     }
-                    return interaction.followUp(`Winner is <@${collected.first().author.id}>!`)
+                    const winnerEmbed = new EmbedBuilder()
+                    .setColor(0x0099FF)
+                    .setTitle('Winner')
+                    .setDescription(`<@${winner}>`)
+                    return interaction.channel.send({ embeds: [winnerEmbed] })
                 })
                 .catch(collected => {
                     interaction.followUp(`<@${user.id}> didnt respond!`)
